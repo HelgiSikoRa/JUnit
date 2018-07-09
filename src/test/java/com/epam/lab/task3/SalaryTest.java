f894db753508d8445712b923a996d74d4f266979
Oleh Sikorskyi <helgi.sikora@gmail.com>
2018-07-09 16:46:54 +0300

    Task5 bug fixed

--- src/test/java/com/epam/lab/task3/SalaryTest.java
+++ src/test/java/com/epam/lab/task3/SalaryTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.Description;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class SalaryTest extends TestWatcher {
+public class SalaryTest {
     private static Person person;
     private static Salary salary;
     private final static double THE_SAME_VALUE = 0.00;
